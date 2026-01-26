6dc28adffeadbb51c21f003bd367cbfbe5ed6032
double-jean <double-jean@users.noreply.github.com>
2026-01-26 13:11:14 +0000

    Deploy preview for PR 133 🛫

---
+++ pr-preview/pr-133/assets/js/menu.js
@@ -0,0 +1,9 @@
+//~ const burger = document.querySelector('#burger');
+//~ const menu = document.querySelector('#menu');
+
+//~ // Set initial state (if not already set in HTML)
+//~ menu.style.visibility = 'hidden';
+
+//~ burger.addEventListener('click', (e) => {
+  //~ menu.style.visibility = menu.style.visibility === 'hidden' ? 'visible' : 'hidden';
+//~ });
