565ac7d373194136fa8d3ca87179785f7025da23
gugray <gugray@users.noreply.github.com>
2026-05-02 12:43:55 +0000

    Deploy preview for PR 134 🛫

---
+++ pr-preview/pr-134/assets/js/future_events.js
@@ -0,0 +1,16 @@
+// from: http://jekyllcodex.org/without-plugin/future-dates/
+
+function getCompareDate() {
+    var d = new Date(),
+        month = '' + (d.getMonth() + 1),
+        day = '' + d.getDate(),
+        year = d.getFullYear();
+    if (month.length < 2) month = '0' + month;
+    if (day.length < 2) day = '0' + day;
+    return [year, month, day].join('');
+}
+
+var elements = document.querySelectorAll('[future-date]');
+Array.prototype.forEach.call(elements, function(el, i){
+    if(el.getAttribute('future-date').split('-').join('') < getCompareDate()) el.remove();
+});
